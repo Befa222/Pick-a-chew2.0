59041b597b14f57ce640fd53736b6b288649a648
Befa222 <72620186+Befa222@users.noreply.github.com>
2021-07-15 13:24:24 +0100

    test

--- src/routes/Ingredients.js
+++ src/routes/Ingredients.js
@@ -55,7 +55,7 @@ export default function Ingredients() {
                     </div>
                 </div>
 
-            <div className='test'>
+           
                 <div className='ingredients-search'>
                     {getIngredients &&
                         <Multiselect
@@ -71,7 +71,7 @@ export default function Ingredients() {
 
                         />
                     }
-                </div>
+              
                 </div>
                 <Link to='/Receipes'><button onClick={play} className='next-button'>NEXT</button></Link>
             </div>
